96da99574246a282809806854b1fad48f58d1008
shivaprasad87 <38426537+shivaprasad87@users.noreply.github.com>
2019-07-29 17:06:32 +0530

    removed online lead drop down

--- application/views/inc/header_nav.php
+++ application/views/inc/header_nav.php
@@ -56,10 +56,10 @@ $baseURL = ($this->session->userdata('user_type') == 'admin') ? base_url('admin'
                                 {
                                    echo '<li><a href="'.$baseLink.'">'.$cModule['module'].'</a></li>';
                                 }
-                                if($cModule['module']== 'Online Lead Report')
+                               /* if($cModule['module']== 'Online Lead Report')
                                 {
                                 echo '<li><a href="'.$baseLink.'">'.$cModule['module'].'</a></li>';
-                                }
+                                }*/
                         }
                         echo '</ul>';
                     }
